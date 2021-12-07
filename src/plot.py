0633198b7a4e12ba3b2e08c902c7c768c5792958
dino <sandhraprathap@gmail.com>
2021-12-07 13:52:38 +0530

    calculation of transmission power

---
+++ src/plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import math as m
+import numpy as np
+c=3*(10**8)
+txantgain=2
+d=np.linspace(500,2425,2425)*(10**3)
+print(d)
+a=20*np.log10(d)
+b=20*m.log10(868*(10**6))
+e=20*m.log10((4*m.pi)/c)
+fspl=a+b+e
+power=20-fspl+txantgain
+plt.plot(d/(10**3),power)
+plt.xlabel("distance(km)")
+plt.ylabel("power(dBm)")
+plt.show()
