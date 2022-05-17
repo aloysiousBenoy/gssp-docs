9169ffbf69354313e61242ae66899e5173112cdc
Aloysious Benoy <aloysiousbenoy@gmail.com>
2022-05-18 00:19:08 +0530

    Added RSSI plts

---
+++ src/TEST-1/rssi.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def decode_rssi_byte(file_name):
+    rssi_list = np.array([])
+    for line in file_name:
+        ind = line.find('\\x')
+        if ind != -1:
+            rssi_list = np.insert(rssi_list, len(
+                rssi_list), int(line[-4:-2], base=16))
+    plt.plot(rssi_list)
+    plt.show()
+
+
+# t1_file = open("./t1.txt")
+# decode_rssi_byte(t1_file)
+# t1_file.close()
+
+
+t1_file = open("./t2.txt")
+decode_rssi_byte(t1_file)
+t1_file.close()
+
+t1_file = open("./rssi-org.txt")
+decode_rssi_byte(t1_file)
+t1_file.close()
